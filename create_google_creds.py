62b63a3e992e2130cebf43e4b16aa31e3260f8f0
Collect999 <dregz299@gmail.com>
2024-06-25 03:30:37 +0100

    add in app.yaml and google creds file making

---
+++ create_google_creds.py
@@ -0,0 +1,17 @@
+# use it like this
+# cat ttsandtranslate-7dd2e2d80d42.json | jq -c .
+
+import os
+
+filename = os.getenv("GOOGLE_CREDS_PATH")
+
+def create_google_creds_file():
+    google_creds_json = os.getenv("GOOGLE_CREDS_JSON")
+    if not google_creds_json:
+        raise ValueError("GOOGLE_CREDS_JSON environment variable is not set")
+    
+    with open(filename, "w") as f:
+        f.write(google_creds_json)
+
+if __name__ == "__main__":
+    create_google_creds_file()
